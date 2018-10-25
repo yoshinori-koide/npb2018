8f8fd50e19d731f4f7c816941e98d9cf02b591f0
Yoshinori Koide <yoshinori.koide@gmail.com>
2018-10-25 23:43:00 +0900

    setup.py に異存パッケージとして dateparser を追記

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+requires = ['dateparser']
+
 setup(
     name         = 'project',
     version      = '1.0',
     packages     = find_packages(),
+    install_requires = requires,
     entry_points = {'scrapy': ['settings = A353949.settings']},
 )
